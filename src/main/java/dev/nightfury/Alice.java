06f3ebd9e49236f216a08f69378ea9cd73b3c67c
TimoDragon <timo.kruse@hebk.de>
2024-03-11 20:21:57 +0100

    BB84 finished

--- src/main/java/dev/nightfury/Alice.java
+++ src/main/java/dev/nightfury/Alice.java
@@ -18,23 +18,41 @@ public class Alice {
     public static void main(String[] args) throws IOException {
         serverSocket = new ServerSocket(25565);
 
-        while (true) {
-            // Warte bis Bob bereit ist die Daten anzunehmen
-            socket = serverSocket.accept();
+        // Warte bis Bob bereit ist die Daten anzunehmen
+        socket = serverSocket.accept();
 
-            // reader um Daten von Bob auszulesen und writer um Daten an Bob zu senden
-            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        // reader um Daten von Bob auszulesen und writer um Daten an Bob zu senden
+        reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-            // Liste mit zufällig generierten QBits
-            List<QBit> bits = generateQBits(20);
+        // Liste mit zufällig generierten QBits
+        List<QBit> bits = generateQBits(20);
 
-            // Sende die QBits zu Bob
-            sendBitsToBob(bits);
+        // Sende die QBits zu Bob
+        sendBitsToBob(bits);
 
-            // Verbindung wird geschlossen
-            closeNetworking();
+        // Bob hat seine Basen an Alice gesendet
+        List<Character> bobBases = getBobBases();
+
+        // Alice vergleicht ihre Basen mit denen von Bob. In diesem Fall übergeben wir einfach die QBits, weil es einfacher zu programmieren ist
+        // da sonst die Basen in eine neue Liste übergeben werden müssen und ich habe keinen Bock drauf weil zu viel Arbeit
+        // Die Liste enthält die Positionen wo die Basen von Bob und Alice gleich sind
+        List<Integer> matching = compareBases(bits, bobBases);
+
+        // Alice sendet die Positionen von den gleichen Basen an Bob
+        sendMatchesToBob(matching);
+
+        // Bob und Alice entfernen die Bits an den jeweiligen Positionen
+        List<Integer> keeped = keepMatchingBits(qbitsToBits(bits), matching);
+
+        // Der Schlüssel wird ausgegeben
+        System.out.println("Der Schlüssel lautet:");
+        for (int i : keeped) {
+            System.out.print(i);
         }
+
+        // Verbindung wird geschlossen
+        closeNetworking();
     }
 
     public static void sendBitsToBob(List<QBit> bits) throws IOException {
@@ -45,6 +63,9 @@ public class Alice {
             writer.newLine();
         }
 
+        // Sende Ende
+        writer.write("END");
+        writer.newLine();
         writer.flush();
     }
 
@@ -52,7 +73,7 @@ public class Alice {
         List<QBit> bits = new LinkedList<>();
 
         SecureRandom random = new SecureRandom();
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < amount; i++) {
             char base = '+';
 
             // 50/50 chance, ob die Basis auf x geändert wird
@@ -66,9 +87,70 @@ public class Alice {
         return bits;
     }
 
+    public static List<Character> getBobBases() throws IOException {
+        List<Character> bases = new LinkedList<>();
+
+        String s = "";
+        while ((s = reader.readLine()) != null) {
+            if (s.equals("END")) {
+                break;
+            }
+
+            bases.add(s.toCharArray()[0]);
+        }
+
+        return bases;
+    }
+
+    public static List<Integer> compareBases(List<QBit> aliceBits, List<Character> bobBases) {
+        List<Integer> matching = new LinkedList<>();
+
+        for (int i = 0; i < aliceBits.size(); i++) {
+            if (aliceBits.get(i).getBase() == bobBases.get(i)) {
+                matching.add(i+1);
+            }
+        }
+
+        return matching;
+    }
+
+    public static void sendMatchesToBob(List<Integer> matches) throws IOException {
+        for (int i : matches) {
+            writer.write(String.valueOf(i));
+            writer.newLine();
+        }
+
+        writer.write("END");
+        writer.newLine();
+        writer.flush();
+    }
+
+    public static List<Integer> qbitsToBits(List<QBit> qBits) {
+        List<Integer> bits = new LinkedList<>();
+
+        for (QBit qBit: qBits) {
+            bits.add(qBit.getBit());
+        }
+
+        return bits;
+    }
+
+    public static List<Integer> keepMatchingBits(List<Integer> bits, List<Integer> matching) {
+        List<Integer> keepedBits = new LinkedList<>();
+
+        for (int i = 0; i < bits.size(); i++) {
+            if (matching.contains(i)) {
+                keepedBits.add(bits.get(i));
+            }
+        }
+
+        return keepedBits;
+    }
+
     public static void closeNetworking() throws IOException {
         reader.close();
         writer.close();
         socket.close();
+        serverSocket.close();
     }
 }
--- src/main/java/dev/nightfury/Bob.java
+++ src/main/java/dev/nightfury/Bob.java
@@ -22,12 +22,27 @@ public class Bob {
         writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
         // Bits von Alice
-        List<QBit> bits = readBitsFromAlice();
+        List<QBit> qbits = readBitsFromAlice();
         // Zufällig generierte Basen von Bob
-        List<Character> bobBases = generateBasis(bits.size());
+        List<Character> bobBases = generateBasis(qbits.size());
 
         // Bob vergleicht die Bits von Alice mit seiner Basis und erstellt den Schlüssel
-        List<Integer> key = generateKey(bits, bobBases);
+        List<Integer > comparedBits = compare(qbits, bobBases);
+
+        // Bob sendet seine Basen an Alice. Dies passiert über einen offenen Kanal
+        sendBasesToAlice(bobBases);
+
+        // Bob liest auf die Antwort von Alice, welche Basen gleich sind und welche nicht
+        List<Integer> matching = readBaseMatches();
+
+        // Bob und Alice entfernen die Bits an den jeweiligen Positionen
+        List<Integer> keeped = keepMatchingBits(qbitsToBits(qbits), matching);
+
+        // Der Schlüssel wird ausgegeben
+        System.out.println("Der Schlüssel lautet:");
+        for (int i : keeped) {
+            System.out.print(i);
+        }
 
         // Verbindung wird geschlossen
         closeNetworking();
@@ -39,6 +54,10 @@ public class Bob {
 
         String s = "";
         while ((s = reader.readLine()) != null) {
+            if (s.equals("END")) { // Ende von Alice
+                break;
+            }
+
             bits.add(gson.fromJson(s, QBit.class));
         }
 
@@ -52,6 +71,7 @@ public class Bob {
         for (int i = 0; i < amount; i++) {
             char b = '+';
 
+            // 50/50 chance, ob die Basis auf x geändert wird
             if (random.nextBoolean()) {
                 b = 'x';
             }
@@ -62,9 +82,70 @@ public class Bob {
         return bobBases;
     }
 
-    public static List<Integer> generateKey(List<QBit> bits, List<Character> bobBases) {
-        // TODO: Fertig machen
-        return null;
+    public static List<Integer> compare(List<QBit> bits, List<Character> bobBases) {
+        List<Integer> bitList = new LinkedList<>();
+        SecureRandom random = new SecureRandom();
+
+        for (int i = 0; i < bits.size(); i++) {
+            QBit qbit = bits.get(i);
+            char base = bobBases.get(i);
+
+            if (qbit.getBase() == base) { // Die Basis von Alice ist dieselbe wie die von Bob
+                bitList.add(qbit.getBit());
+            } else { // Die Basis von Alice ist anders als von Bob was dafür sorgt, dass Bobs messung zufällig ist
+                bitList.add(random.nextInt(2));
+            }
+        }
+
+        return bitList;
+    }
+
+    public static void sendBasesToAlice(List<Character> bases) throws IOException {
+        for (char c : bases) {
+            writer.write(String.valueOf(c));
+            writer.newLine();
+        }
+
+        writer.write("END");
+        writer.newLine();
+        writer.flush();
+    }
+
+    public static List<Integer> readBaseMatches() throws IOException {
+        List<Integer> matches = new LinkedList<>();
+
+        String s = "";
+        while ((s = reader.readLine()) != null) {
+            if (s.equals("END")) {
+                break;
+            }
+
+            matches.add(Integer.parseInt(s));
+        }
+
+        return matches;
+    }
+
+    public static List<Integer> qbitsToBits(List<QBit> qBits) {
+        List<Integer> bits = new LinkedList<>();
+
+        for (QBit qBit: qBits) {
+            bits.add(qBit.getBit());
+        }
+
+        return bits;
+    }
+
+    public static List<Integer> keepMatchingBits(List<Integer> bits, List<Integer> matching) {
+        List<Integer> keepedBits = new LinkedList<>();
+
+        for (int i = 0; i < bits.size(); i++) {
+            if (matching.contains(i)) {
+                keepedBits.add(bits.get(i));
+            }
+        }
+
+        return keepedBits;
     }
 
     public static void closeNetworking() throws IOException {
